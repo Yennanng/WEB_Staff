476e8eee9823571e6fda796d24281156d3a02abd
Web_PawCare <yennan275@gmail.com>
2026-07-11 19:02:31 +0700

    Bypass ESLint in production build on Vercel

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
   /* config options here */
   reactStrictMode: true,
   allowedDevOrigins: ['10.0.213.117'],
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
