d309a0c6aed3a7ef03be342690dafbf5ca4c5df7
Web_PawCare <yennan275@gmail.com>
2026-07-11 19:08:00 +0700

    Properly bypass ESLint for Next 16 by renaming eslint config

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -3,9 +3,6 @@ const nextConfig = {
   /* config options here */
   reactStrictMode: true,
   allowedDevOrigins: ['10.0.213.117'],
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
 };
 
 export default nextConfig;
